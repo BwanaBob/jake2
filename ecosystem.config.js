92ebabced710ae86cb67bd1d5ede7c496b7bd9b1
BwanaBob <bowanabob@yahoo.com>
2024-01-25 14:12:40 -0600

    PM2 update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   apps : [{
     name   : "jake2",
-    script : "./app.js"
-  }]
+    script : "./index.js"
+  }],
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      max_restarts : 5,
+      min_uptime : 30,
+      restart_delay: 5000,
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
 }
